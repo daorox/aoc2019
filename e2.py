4cc521a4dad0b8f332f93426ba3875a0fe92cf67
daorox <daorox@lawl.pw>
2019-12-03 21:10:38 +0100

    Add solutions for day2/day3

---
+++ e2.py
@@ -0,0 +1,38 @@
+from operator import add, mul
+from copy import deepcopy
+
+# 1
+
+
+def parse_opcodes(lst):
+    lst = deepcopy(lst)
+    for i in range(0, len(lst), 4):
+        a = lst[lst[i+1]]
+        b = lst[lst[i+2]]
+        op = add if lst[i] == 1 else mul
+        lst[lst[i+3]] = op(a, b)
+
+        if lst[i+4] == 99:
+            break
+    return lst[0]
+
+
+with open("e2.txt") as f:
+    lst = list(map(int, f.read().split(",")))
+
+lst[1] = 12
+lst[2] = 2
+
+print(parse_opcodes(lst))
+
+# 2
+
+with open("e2.txt") as f:
+    lst = list(map(int, f.read().split(",")))
+for x in range(100):
+    for y in range(100):
+        lst[1] = x
+        lst[2] = y
+
+        if parse_opcodes(lst) == 19690720:
+            print(x, y)
---
+++ e3.py
@@ -0,0 +1,44 @@
+with open("e3.txt") as f:
+    lines = f.read().splitlines()
+
+all_pos_costs = []
+for line in lines:
+    pos = (0, 0)
+    steps = 0
+    # The amount of steps required to reach each position
+    pos_cost = {}
+    for cell in line.split(','):
+        d = cell[0]
+        length = int(cell[1:])
+        if d == "R":
+            new_positions = zip(
+                (pos[0]+x for x in range(1, length+1)), [pos[1]]*length)
+        if d == "U":
+            new_positions = zip(
+                [pos[0]]*length, (pos[1]-x for x in range(1, length+1)))
+        if d == "L":
+            new_positions = zip(
+                (pos[0]-x for x in range(1, length+1)), [pos[1]]*length)
+        if d == "D":
+            new_positions = zip(
+                [pos[0]]*length, (pos[1]+x for x in range(1, length+1)))
+        for new_pos in new_positions:
+            steps += 1
+            if new_pos not in pos_cost:
+                pos_cost[new_pos] = steps
+        pos = new_pos
+    all_pos_costs.append(pos_cost)
+
+
+def dist(x_value, x_goal, y_value, y_goal):
+    return abs(x_value - x_goal) + abs(y_value - y_goal)
+
+
+all_coords = [set(line.keys()) for line in all_pos_costs]
+
+# 1
+print(min(dist(i[0], 0, i[1], 0)
+          for i in all_coords[0].intersection(all_coords[1])))
+# 2
+print(min(all_pos_costs[0][i] + all_pos_costs[1][i]
+          for i in all_coords[0].intersection(all_coords[1])))
